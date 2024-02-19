027ac154e9e82deb711ec20be60775cf702b7658
Artem Vesnin <artemvesnin@gmail.com>
2024-02-19 23:26:17 +0000

    Added zero devision test with pytest.raises

--- tests/test_devider.py
+++ tests/test_devider.py
@@ -9,3 +9,7 @@ def test_devide_numbers():
     assert devide_numbers(1, 2) == 0.5
     assert devide_numbers(-1, 1) == -1
     assert devide_numbers(10, -5) == -2
+
+def test_devide_zero():
+    with pytest.raises(ZeroDivisionError):
+        devide_numbers(1, 0)
