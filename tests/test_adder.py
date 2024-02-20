93f9d5424faf4f953b33c7df2f11db870158900f
Artem Vesnin <artemvesnin@gmail.com>
2024-02-20 00:58:37 +0000

    Added adder test with strings

--- tests/test_adder.py
+++ tests/test_adder.py
@@ -1,4 +1,5 @@
 # tests/test_adder.py
+import pytest
 
 from arithmetics.adder import (
     add_numbers
@@ -8,3 +9,7 @@ def test_add_numbers():
     assert add_numbers(1, 2) == 3
     assert add_numbers(-1, 1) == 0
     assert add_numbers(10, -5) == 5
+
+def test_add_strings():
+    with pytest.raises(ValueError):
+        add_numbers("a", "b")
